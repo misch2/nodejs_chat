3b53a264186beb2f9cc23c7c8f6b2dd110a8dbc6
Michal Schwarz <michal.schwarz@gmail.com>
2020-09-25 21:12:42 +0200

    basic sockets

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io').listen(server); // will also server socket.io.js to client side
 
-// expose everything in this folder for serving over http:
+// expose everything in this folder for serving over http
 app.use(express.static(__dirname + '/public'));
-// manage special '/' URL (otherwise we would get just a 'Cannot GET /' error message):
+// manage special '/' URL (otherwise we would get just a 'Cannot GET /' error message)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8000, () => {
+server.listen(process.env.PORT || 8000, () => {
     console.log('Started server');
+});
+
+io.on('connection', (socket) => {
+    console.log('new connection as %s', socket.id);
+    socket.on('disconnect', () => {
+        console.log('%s disconnected', socket.id);
+    });
 });
 
