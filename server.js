ea69576eb11340dc22e1e29bec0e782d2267e370
Michal Schwarz <michal.schwarz@gmail.com>
2020-09-26 20:53:03 +0200

    persistent history
    newly connected clients will receive recent history

--- server.js
+++ server.js
@@ -15,18 +15,53 @@ server.listen(port, () => {
     console.log('Started server on http://localhost:%d/', port);
 });
 
+var stats = {
+    "users_cnt": 0,
+    "messages_cnt": 0,
+};
+
+var history = [];
+
+function update_stats() {
+    io.emit('stats', stats);    // to all
+};
+
+
+// socket.emit() == to current client
+// socket.broadcast.emit() == to all, excluding current client
+// io.emit() == to all, including current client
 io.on('connection', (socket) => {
     console.log('new connection as %s', socket.id);
-    socket.emit('system-message', 'New user connected: '+socket.id);    // to all, excluding me
+    //socket.broadcast.emit('system-message', 'New user connected: '+socket.id);
+    stats.users_cnt++;
+    update_stats();
+
+    // send full history to this new client:
+    for (var i=0; i < history.length; i++) {
+        socket.emit('chat-message-server', history[i]);
+    };
 
     socket.on('disconnect', () => {
         console.log('%s disconnected', socket.id);
-        io.emit('system-message', 'User disconnected: '+socket.id);
+        //io.emit('system-message', 'User disconnected: '+socket.id);
+        stats.users_cnt--;
+        update_stats();
     });
 
     socket.on('chat-message-client', (data) => {
-        console.debug('%s: got message "%o"', socket.id, data);
-        io.emit('chat-message-server', data);   // to all, including me
+        // when client sends a message, add it to history and send it to all including him/her
+
+        console.log('%s: got message "%o"', socket.id, data);
+        io.emit('chat-message-server', data);
+
+        // keep history
+        history.push(data);
+        if (history.length > 5) {
+            history.shift();
+        }
+        //console.debug('history: %o', history);
+        stats.messages_cnt++;
+        update_stats();
     });
 });
 
